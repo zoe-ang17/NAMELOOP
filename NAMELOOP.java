aec79cd7678e73a3d7b1bc467a8a9e6c24fbd697
Zoe Ang <zwAng@mcm.edu.ph>
2025-03-09 00:07:59 +0800

    Add files via upload

---
+++ NAMELOOP.java
@@ -0,0 +1,9 @@
+public class NAMELOOP {
+    public static void main(String[] args) {
+String[] names = {"Kazutora", "Izana", "Rindou", "Ran", "Manjiro"};
+
+for (String loop : names) {
+    System.out.println("NAME: " + loop);
+        }
+    }
+}
